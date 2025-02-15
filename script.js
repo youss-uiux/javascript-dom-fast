c59cd4fb76646a5b1b21fdc86845fe6e8b7eb9e5
youss-uiux <youssif.aboubacar@outlook.fr>
2025-02-15 12:46:26 +0100

    add interactive button to increment chiffre and update its color

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 document.getElementById('demo').innerHTML = 'Hello JavaScript!';
+var chiffre=document.querySelector('.chiffre').textContent;
+var chiffre=parseInt(chiffre);
+chiffre=chiffre+1;
+document.querySelector('.chiffre').textContent=chiffre;
+
+function plus(){
+    var chiffre=document.querySelector('.chiffre').textContent;
+    var chiffre=parseInt(chiffre);
+    chiffre=chiffre+1;
+    document.querySelector('.chiffre').style.color='red';
+}
