e02f80e5c4cfc31024bdf166fc7519696d7f7320
Youss-zedpay <youssif.aboubacar@zedpay.africa>
2025-03-08 18:28:21 +0100

    update index.html to change stylesheet link and add multiple buttons; enhance script.js with button click events and alerts; create style.css for button styling

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-//nom ="Mariam";
-//alert(nom);
-import {fibonnacci} from 'script.ts'
-console.log(fibonnacci.n);
+function mafonction(){
+    alert("Ma fonction ");
+}
+function afficheVert(){
+    alert("Vert");
+}
+var btn=document.getElementsByTagName("button");
+//alert(btn[0].innerHTML);
+//alert(btn[1].innerHTML);
+var btn1=document.getElementById("btn1");
+var btn2=document.getElementById("btn2");
+document.querySelectorAll('.btn').forEach((btn)=>{
+    btn.addEventListener('click',afficheVert)
+})
+document.querySelectorAll('.btnrouge').forEach((boutton)=>{
+    boutton.addEventListener('click',()=>{
+        alert('rouge');
+    })
+})
+
+document.querySelector('.un').addEventListener('click',()=>{
+    alert(1);
+})
